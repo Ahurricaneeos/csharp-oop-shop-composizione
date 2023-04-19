61e24af425909752486a793e39a0cfe5ad875d6a
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-19 15:18:34 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop-composizione/Product.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace csharp_oop_shop_composizione
+{
+    public class Product
+    {
+        // Attributes
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+        private int iva;
+
+        // Costructor
+        public Product(string nome, string descrizione, double prezzo, int iva)
+        {
+            int codice = new Random().Next(0, 99999999);
+            this.codice = codice;
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+        // Getters
+        public int GetCodice() { return codice; }
+        public string GetNome() { return nome; }
+        public string GetDescrizione() {  return descrizione; }
+        public double GetPrezzo() { return prezzo; }
+        public int GetIva() { return iva; }
+
+        // Setters
+        
+        public void SetNome(string nome) { this.nome = nome; }
+        public void SetIva(int iva) { this.iva = iva; }
+        public void SetDescrizione(string descrizione) { this.descrizione = descrizione; }
+        public void SetPrezzo(double prezzo) { this.prezzo = prezzo; }
+
+        // Methods
+        private string PrezzoPlusIva()
+        {
+            double ivaSulPrezzo = prezzo + (prezzo * iva / 100);
+            string Iva = ivaSulPrezzo.ToString();
+            return Iva;
+        }
+
+        private string NomeEsteso()
+        {
+            string nomePiuCodice = codice + "-" + nome;
+            return nomePiuCodice;
+        }
+
+        public void StampaProdottiSenzaIva()
+        {
+            Console.WriteLine("Codice: " + codice);
+            Console.WriteLine("Nome prodotto: " + nome);
+            Console.WriteLine("Descrizione prodotto: " + descrizione);
+            Console.WriteLine("Prezzo: " + prezzo + "€");
+        }
+
+        public void StampaProdottiConIva()
+        {
+            Console.WriteLine("Codice: " + codice);
+            Console.WriteLine("Nome prodotto: " + nome);
+            Console.WriteLine("Descrizione prodotto: " + descrizione);
+            Console.WriteLine("IVA: " + iva + "%");
+            Console.WriteLine("Prezzo con iva: " + PrezzoPlusIva() + "€");
+        }
+
+        public void StampaProdottiConcatenati()
+        {
+            Console.WriteLine("Codice e nome del prodotto: " + NomeEsteso());
+            Console.WriteLine("Descrizione prodotto: " + descrizione);
+            Console.WriteLine("IVA: " + iva + "%");
+            Console.WriteLine("Prezzo con iva: " + PrezzoPlusIva() + "€");
+        }
+    }
+}
---
+++ csharp-oop-shop-composizione/Shop.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop_composizione
+{
+    public class Shop
+    {
+        // Attributes
+        private string name;
+        private string city;
+        private string address;
+        private int civic;
+        private List<Product> products;
+
+        // Costructor
+        public Shop(string name, string city, string address, int civic)
+        {
+            this.name = name;
+            this.city = city;
+            this.address = address;
+            this.civic = civic;
+            this.products = new List<Product>();
+        }
+
+        // Getters
+
+        public string GetName() { return name; }
+        public string GetCity() { return city; }
+        public string GetAddress() { return address; }
+        public int GetCivic() { return civic; }
+        public List<Product> GetListProducts() { return products; }
+
+        // Setters
+
+        public void SetCity(string city) { this.city = city;}
+        public void SetAddress(string address) {  this.address = address;}
+        public void SetCivic(int civic) { this.civic = civic;}
+
+        // Methods
+
+        public void addProduct(Product newProduct) { products.Add(newProduct); }
+
+        public void addListProducts(List<Product> newListProducts)
+        {
+            foreach (Product newListProduct in newListProducts) { products.Add(newListProduct);}
+        }
+        
+        public string stringRappresentation()
+        {
+            string rapprStr = "Nome negozio: " + this.name + "\n";
+            rapprStr += "Città: " + this.city + "\n";
+            rapprStr += "Indirizzo: " + this.address + " ";
+            rapprStr += "Numero civico: " + this.civic + "\n";
+​
+            foreach (Product scanProduct in products)
+            {
+                rapprStr += " - " + scanProduct.GetProducts() + "\n";
+            }
+​
+            rapprStr += "\n";
+​
+            return rapprStr;
+        }
+    }
+
+    
+}
